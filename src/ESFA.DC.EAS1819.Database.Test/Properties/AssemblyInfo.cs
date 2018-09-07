d2bca7c6ab7fc169cf2ed9d0edede9b1e4ad4c7f
Hari Dupati <dupatihari@gmail.com>
2018-09-07 10:41:36 +0100

    58935 - Added database schema tests

---
+++ src/ESFA.DC.EAS1819.Database.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ESFA.DC.EAS1819.Database.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ESFA.DC.EAS1819.Database.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32b0aadd-b3b4-49c9-87e7-2ef50064e145")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
