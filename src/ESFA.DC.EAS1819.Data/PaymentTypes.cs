8113a60a3af4ae4777dfa63e05c5c465df4ee7ce
Hari Dupati <dupatihari@gmail.com>
2018-09-14 07:54:20 +0100

    Added new Payment Type columns to dbcontext and added dbo schema tests

--- src/ESFA.DC.EAS1819.Data/PaymentTypes.cs
+++ src/ESFA.DC.EAS1819.Data/PaymentTypes.cs
@@ -7,5 +7,10 @@ namespace ESFA.DC.EAS1819.Data
     {
         public int PaymentId { get; set; }
         public string PaymentName { get; set; }
+
+        public bool FM36 { get; set; }
+        public string SubSectionHeading { get; set; }
+        public string RowHeading { get; set; }
+        public string PaymentTypeDescription { get; set; }
     }
 }
