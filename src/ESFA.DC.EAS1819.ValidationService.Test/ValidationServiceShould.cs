3d8ea5f168816cf182282df524dc104f126e27ff
Hari Dupati <dupatihari@gmail.com>
2018-11-26 09:48:58 +0000

    64674 - Added ValidationErrorRuleBuilder

--- src/ESFA.DC.EAS1819.ValidationService.Test/ValidationServiceShould.cs
+++ src/ESFA.DC.EAS1819.ValidationService.Test/ValidationServiceShould.cs
@@ -10,6 +10,7 @@ using ESFA.DC.EAS1819.DataService.Interface.FCS;
 using ESFA.DC.EAS1819.EF;
 using ESFA.DC.EAS1819.Service;
 using ESFA.DC.EAS1819.Tests.Base.Builders;
+using ESFA.DC.EAS1819.ValidationService.Builders;
 using Moq;
 using Xunit;
 
@@ -22,6 +23,7 @@ namespace ESFA.DC.EAS1819.ValidationService.Test
         private readonly EasValidationService _validationService;
         private readonly Mock<IEasPaymentService> _easPaymentServiceMock;
         private readonly Mock<IValidationErrorService> _validationErrorServiceMock;
+        private readonly Mock<IValidationErrorRuleService> _validationErrorRuleServiceMock;
         private readonly Mock<IDateTimeProvider> _dateTimeProviderMock;
 
         public ValidationServiceShould()
@@ -31,18 +33,23 @@ namespace ESFA.DC.EAS1819.ValidationService.Test
             _dateTimeProviderMock = new Mock<IDateTimeProvider>();
             _fcsDataServiceMock = new Mock<IFCSDataService>();
             _fundingLineContractTypeMock = new Mock<IFundingLineContractTypeMappingDataService>();
+            _validationErrorRuleServiceMock = new Mock<IValidationErrorRuleService>();
 
             _fcsDataServiceMock.Setup(x => x.GetContractsForProvider(It.IsAny<int>())).Returns(new ContractAllocationsBuilder().Build);
             _fundingLineContractTypeMock.Setup(x => x.GetAllFundingLineContractTypeMappings()).Returns(new FundingLineContractTypeMappingsBuilder().Build);
             _easPaymentServiceMock.Setup(x => x.GetAllPaymentTypes()).Returns(new PaymentTypesBuilder().GetPaymentTypeList);
             _dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(DateTime.UtcNow);
 
+            _validationErrorRuleServiceMock.Setup(x => x.GetAllValidationErrorRules())
+                .Returns(new ValidationErrorRuleBuilder().Build());
+
             _validationService = new EasValidationService(
                 _easPaymentServiceMock.Object,
                 _dateTimeProviderMock.Object,
                 _validationErrorServiceMock.Object,
                 _fcsDataServiceMock.Object,
-                _fundingLineContractTypeMock.Object);
+                _fundingLineContractTypeMock.Object,
+                _validationErrorRuleServiceMock.Object);
         }
 
         [Fact]
