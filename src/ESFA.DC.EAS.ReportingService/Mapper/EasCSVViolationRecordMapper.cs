8bcad2752454b7eb740971cc1d4dcd8b447bda74
Hari Dupati <dupatihari@gmail.com>
2019-07-19 22:45:00 +0100

    Funding and Violation Reports and some tydiing up.

--- src/ESFA.DC.EAS.ReportingService/Mapper/EasCSVViolationRecordMapper.cs
+++ src/ESFA.DC.EAS.ReportingService/Mapper/EasCSVViolationRecordMapper.cs
@@ -19,6 +19,7 @@ namespace ESFA.DC.EAS.ReportingService.Mapper
             Map(m => m.CalendarYear).Name("CalendarYear");
             Map(m => m.CalendarMonth).Name("CalendarMonth");
             Map(m => m.Value).Name("Value");
+            Map(m => m.DevolvedAreaSoF).Name("DevolvedAreaSourceOfFunding");
             Map(m => m.OfficialSensitive).Name("OFFICIAL-SENSITIVE");
         }
     }
