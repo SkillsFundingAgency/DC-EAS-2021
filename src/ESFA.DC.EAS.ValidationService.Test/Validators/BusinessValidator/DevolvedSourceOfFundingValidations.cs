c7a492b6c141a6a97a6fba13a68e40511230a466
Hari Dupati <dupatihari@gmail.com>
2019-07-22 23:10:39 +0100

    DevolvedSourceOfFunding_03 unit tests

--- src/ESFA.DC.EAS.ValidationService.Test/Validators/BusinessValidator/DevolvedSourceOfFundingValidations.cs
+++ src/ESFA.DC.EAS.ValidationService.Test/Validators/BusinessValidator/DevolvedSourceOfFundingValidations.cs
@@ -8,21 +8,26 @@ namespace ESFA.DC.EAS.ValidationService.Test.Validators.BusinessValidator
 {
     public class DevolvedSourceOfFundingValidations : BusinessValidatorBase
     {
+        private EasCsvRecord easRecord;
+        public DevolvedSourceOfFundingValidations()
+        {
+            easRecord = new EasCsvRecord()
+            {
+                CalendarMonth = "8",
+                CalendarYear = "2019",
+                Value = "1",
+                AdjustmentType = "adjustmentType"
+            };
+            dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(new DateTime(2019, 09, 01));
+        }
+
         [Theory]
         [InlineData("0")]
         [InlineData("-10")]
         public void HaveError_When_NotAValidLookUp(string devolvedAreaSourceOfFunding)
         {
-            var easRecord = new EasCsvRecord()
-            {
-                CalendarMonth = "8",
-                CalendarYear = "2019",
-                Value = "1",
-                FundingLine = "fundline",
-                DevolvedAreaSourceOfFunding = devolvedAreaSourceOfFunding,
-                AdjustmentType = "adjustmentType"
-            };
-            dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(new DateTime(2019, 09, 01));
+            easRecord.FundingLine = "fundingLine";
+            easRecord.DevolvedAreaSourceOfFunding = devolvedAreaSourceOfFunding;
             _validator = new BusinessRulesValidator(_contractAllocations, _fundingLineContractTypeMappings, paymentTypes, dateTimeProviderMock.Object, 1);
             _validator.ShouldHaveValidationErrorFor(x => x.DevolvedAreaSourceOfFunding, easRecord).WithErrorCode("DevolvedAreaSourceOfFunding_03");
         }
@@ -38,16 +43,8 @@ namespace ESFA.DC.EAS.ValidationService.Test.Validators.BusinessValidator
         [InlineData("116")]
         public void NotHaveError_When_DevolvedAreaSourceOfFunding_IsValid(string devolvedAreaSourceOfFunding)
         {
-            var easRecord = new EasCsvRecord()
-            {
-                CalendarMonth = "8",
-                CalendarYear = "2019",
-                Value = "1",
-                FundingLine = "fundline",
-                DevolvedAreaSourceOfFunding = devolvedAreaSourceOfFunding,
-                AdjustmentType = "adjustmentType"
-            };
-            dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(new DateTime(2019, 09, 01));
+            easRecord.FundingLine = "fundingLine";
+            easRecord.DevolvedAreaSourceOfFunding = devolvedAreaSourceOfFunding;
             _validator = new BusinessRulesValidator(_contractAllocations, _fundingLineContractTypeMappings, paymentTypes, dateTimeProviderMock.Object, 1);
             var result = _validator.Validate(easRecord);
             Assert.DoesNotContain(result.Errors, x => x.ErrorCode.Equals("DevolvedAreaSourceOfFunding_03"));
@@ -57,18 +54,10 @@ namespace ESFA.DC.EAS.ValidationService.Test.Validators.BusinessValidator
         [Theory]
         [InlineData("Adult Education - Eligible for MCA/GLA funding (non-procured)")]
         [InlineData("Adult Education - Eligible for MCA/GLA funding (procured)")]
-        public void HaveError_When_DevolvedSourceOfFundingDoesNotExist_For_Required_Fundlines(string fundline)
+        public void HaveError_When_DevolvedSourceOfFundingDoesNotExist_For_Required_Fundlines(string fundingLine)
         {
-            var easRecord = new EasCsvRecord()
-            {
-                CalendarMonth = "8",
-                CalendarYear = "2019",
-                Value = "1",
-                FundingLine = fundline,
-                DevolvedAreaSourceOfFunding = null,
-                AdjustmentType = "adjustmentType"
-            };
-            dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(new DateTime(2019, 09, 01));
+            easRecord.FundingLine = fundingLine;
+            easRecord.DevolvedAreaSourceOfFunding = null;
             _validator = new BusinessRulesValidator(_contractAllocations, _fundingLineContractTypeMappings, paymentTypes, dateTimeProviderMock.Object, 1);
             _validator.ShouldHaveValidationErrorFor(x => x.DevolvedAreaSourceOfFunding, easRecord).WithErrorCode("DevolvedAreaSourceOfFunding_01");
         }
@@ -77,21 +66,36 @@ namespace ESFA.DC.EAS.ValidationService.Test.Validators.BusinessValidator
         [Theory]
         [InlineData("Adult Education - Eligible for MCA/GLA funding (non-procured)", "110")]
         [InlineData("Adult Education - Eligible for MCA/GLA funding (procured)", "116")]
-        public void NotHaveError_When_DevolvedSourceOfFundingExist_For_Required_Fundlines(string fundline, string devolvedSourceOfFunding)
+        public void NotHaveError_When_DevolvedSourceOfFundingExist_For_Required_Fundlines(string fundingLine, string devolvedSourceOfFunding)
         {
-            var easRecord = new EasCsvRecord()
-            {
-                CalendarMonth = "8",
-                CalendarYear = "2019",
-                Value = "1",
-                FundingLine = fundline,
-                DevolvedAreaSourceOfFunding = devolvedSourceOfFunding,
-                AdjustmentType = "adjustmentType"
-            };
-            dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(new DateTime(2019, 09, 01));
+            easRecord.FundingLine = fundingLine;
+            easRecord.DevolvedAreaSourceOfFunding = devolvedSourceOfFunding;
             _validator = new BusinessRulesValidator(_contractAllocations, _fundingLineContractTypeMappings, paymentTypes, dateTimeProviderMock.Object, 1);
             var result = _validator.Validate(easRecord);
             Assert.DoesNotContain(result.Errors, x => x.ErrorCode.Equals("DevolvedAreaSourceOfFunding_01"));
+        }
+
+        [Theory]
+        [InlineData("Fundingline1", "100")]
+        [InlineData("Fundingline2", "116")]
+        public void HaveError_When_DevolvedSourceOfFundingExist_For_NonRequired_Fundlines(string fundingLine, string devolvedSourceOfFunding)
+        {
+            easRecord.FundingLine = fundingLine;
+            easRecord.DevolvedAreaSourceOfFunding = devolvedSourceOfFunding;
+            _validator = new BusinessRulesValidator(_contractAllocations, _fundingLineContractTypeMappings, paymentTypes, dateTimeProviderMock.Object, 1);
+            _validator.ShouldHaveValidationErrorFor(x => x.DevolvedAreaSourceOfFunding, easRecord).WithErrorCode("DevolvedAreaSourceOfFunding_02");
+        }
+
+        [Theory]
+        [InlineData("Fundingline1", null)]
+        [InlineData("Fundingline2", "")]
+        public void NotHaveError_When_DevolvedSourceOfFunding_DoesNotExist_For_NonRequired_Fundlines(string fundingLine, string devolvedSourceOfFunding)
+        {
+            easRecord.FundingLine = fundingLine;
+            easRecord.DevolvedAreaSourceOfFunding = devolvedSourceOfFunding;
+            _validator = new BusinessRulesValidator(_contractAllocations, _fundingLineContractTypeMappings, paymentTypes, dateTimeProviderMock.Object, 1);
+            var result = _validator.Validate(easRecord);
+            Assert.DoesNotContain(result.Errors, x => x.ErrorCode.Equals("DevolvedAreaSourceOfFunding_02"));
         }
     }
 }
