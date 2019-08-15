7312c121e3be6bcafe6704e847b4cedb6c92bc10
Peter Chamley <pete.chamley@education.gov.uk>
2019-08-15 12:50:07 +0000

    Add update to Pre Scrip to check if tables exists, incase db never deployed. remove Bin folder for project file

--- src/ESFA.DC.EAS1920.Database/zz.Script.PreDeployment.sql
+++ src/ESFA.DC.EAS1920.Database/zz.Script.PreDeployment.sql
@@ -6,8 +6,11 @@ Pre-Deployment Script
 SET NOCOUNT ON ;
 
 GO
-UPDATE SV
-	SET [DevolvedAreaSoF] = -1
-FROM [dbo].[EAS_Submission_Values] SV 
-WHERE SV.[DevolvedAreaSoF] IS NULL
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[EAS_Submission_Values]') AND type in (N'U'))
+BEGIN 
+	UPDATE SV
+		SET [DevolvedAreaSoF] = -1
+	FROM [dbo].[EAS_Submission_Values] SV 
+	WHERE SV.[DevolvedAreaSoF] IS NULL
+END
 GO
