2f798e4d3955af5feaa74f631a06005533819346
Peter Chamley <pete.chamley@education.gov.uk>
2019-08-15 12:31:05 +0000

    Add PreDeployment Script to Set Default Value for records already in DB. This should allow chaning the column to NOT NULL. EAS_Submission_Values table match current syntax for not null and default values. Add SP to display extened Props. Moved Extended Props to Seperate file (as per other DBs) Rename Roles  (as per other DBs)

---
+++ src/ESFA.DC.EAS1920.Database/zz.Script.PreDeployment.sql
@@ -0,0 +1,13 @@
+﻿/*
+Pre-Deployment Script 
+--------------------------------------------------------------------------------------
+*/
+
+SET NOCOUNT ON ;
+
+GO
+UPDATE SV
+	SET [DevolvedAreaSoF] = -1
+FROM [dbo].[EAS_Submission_Values] SV 
+WHERE SV.[DevolvedAreaSoF] IS NULL
+GO
